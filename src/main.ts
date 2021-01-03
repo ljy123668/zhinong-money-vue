a36e2c58c49e07666695e89b55d6e797cca5a2b7
zhinong <651507008@qq.com>
2021-01-03 19:50:17 +0800

    添加svg 图片和配置

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import router from './router'
 import store from './store'
 
 import Layout from '@/components/Layout.vue'
+import Icon from '@/components/Icon.vue'
 
 Vue.config.productionTip = false
 
 
 Vue.component('Layout',Layout)
+Vue.component('Icon',Icon)
 
 new Vue({
   router,
