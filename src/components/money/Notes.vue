c13fb29a8f063a796ad7a07072845750a805b710
zhinong <651507008@qq.com>
2021-01-04 16:49:33 +0800

    完成 Notes组件

--- src/components/money/Notes.vue
+++ src/components/money/Notes.vue
@@ -1,35 +1,38 @@
 <template>
-     <div>
-            <label class="notes">
-                <span class="name">备注</span>
-                <input type="text" placeholder="在这里输入备注">
-            </label>
-        </div>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" v-model="value" placeholder="在这里输入备注" />
+    </label>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  value = "";
+}
 </script>
 
 <style lang="scss" scoped>
-.notes{
-    font-size: 14px;
-    background: #f5f5f5;
-    display: block;
-    padding-left: 16px;
-    display: flex;
-    align-items: center;
-    .name{
-        padding-right:16px
-    }
-    input{
-        height: 64px;
-        flex-grow:1;
-        background: transparent;
-        border: none;
-    }
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: block;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+  }
 }
-
 </style>
