e2f9eb1b8f1f23b11dca8fb8bf11ef3099a0b29b
zhinong <651507008@qq.com>
2021-01-04 17:07:54 +0800

    完成选择标签

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -1,67 +1,77 @@
 <template>
-      <div class="tags">
-               <div class="new">
-                <button>新增标签</button>
-            </div>
-            <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ul>
-         
-        </div>
-      
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li
+        v-for="tag in dataSource"
+        :key="tag"
+        @click="toggle(tag)"
+        :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
+      >
+        {{ tag }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script>
-    export default {
-        
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue {
+  @Prop() dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
     }
+
+    console.log(this.selectedTags);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.tags{
-    font-size: 14px;
-    padding: 16px;
-    flex-grow: 1;
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
     display: flex;
-     flex-direction: column-reverse;
-    > .current{
-        display: flex;
-        flex-wrap: wrap;
-        > li{
-            background-color: #d9d9d9;
-            $h:24px;
-            height: $h;
-            line-height: $h;
-            border-radius: $h/2;
-            padding:  0 16px;
-            margin-right:12px;
-            margin-top: 4px;
-        }
+    flex-wrap: wrap;
+    > li {
+      $bg: #d9d9d9;
+      background-color: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+      &.selected {
+        background: darken($bg, 50%);
+        color: #fff;
+      }
     }
-    > .new{
-        padding-top:16px;
-        button{
-            background: transparent;
-            border: none;
-            color:#999;
-            border-bottom:1px solid;
-            padding: 0 4px;
-        }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
     }
+  }
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,26 +1,34 @@
 <template>
-    <Layout class-prefix='layout'>
-        <NumberPad />
-        <Types />
-        <Notes />
-        <Tags />
-    </Layout>
+  <Layout class-prefix="layout">
+    <NumberPad />
+    <Types />
+    <Notes />
+    <Tags :data-source="tags" />
+  </Layout>
 </template>
 
 <script>
-    import Tags from '@/components/money/Tags'
-    import Notes from '@/components/money/Notes'
-    import NumberPad from '@/components/money/NumberPad'
-    import Types from '@/components/money/Types'
-    export default {
-        components:{
-            Tags,Notes,NumberPad,Types
-        }
-    }
+import Tags from "@/components/money/Tags";
+import Notes from "@/components/money/Notes";
+import NumberPad from "@/components/money/NumberPad";
+import Types from "@/components/money/Types";
+export default {
+  components: {
+    Tags,
+    Notes,
+    NumberPad,
+    Types,
+  },
+  data() {
+    return {
+      tags: ["衣", "食", "住", "行", "玩"],
+    };
+  },
+};
 </script>
 <style lang="scss">
-    .layout-content{
-    display: flex;
-    flex-direction: column-reverse;
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
 }
 </style>
