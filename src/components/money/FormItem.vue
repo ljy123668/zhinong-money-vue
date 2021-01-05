d64b4e6e092f452b3d329dd9cdbdeef89d065eab
zhinong <651507008@qq.com>
2021-01-05 16:33:13 +0800

    修复value 赋值问题

--- src/components/money/FormItem.vue
+++ src/components/money/FormItem.vue
@@ -2,7 +2,12 @@
   <div>
     <label class="fromItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder" />
+      <input
+        type="text"
+        :value="value"
+        @input="onValueVhanged($event.target.value)"
+        :placeholder="this.placeholder"
+      />
     </label>
   </div>
 </template>
@@ -13,7 +18,7 @@ import { Component, Watch, Prop } from "vue-property-decorator";
 
 @Component
 export default class FormItem extends Vue {
-  value = "";
+  @Prop({ default: "" }) value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
   @Watch("value")
--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -6,7 +6,7 @@
     <ul class="current">
       <li
         v-for="tag in dataSource"
-        :key="tag"
+        :key="tag.id"
         @click="toggle(tag)"
         :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
       >
