69a8326b1ceeb7934a7d474a5dea31f08be44ca7
zhinong <651507008@qq.com>
2021-01-07 15:42:01 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,16 +5,20 @@
       :data-source="recordTypeList"
       :value.sync="type"
     />
-    <Tabs
-      class-prefix="interval"
-      :data-source="intervalList"
-      :value.sync="interval"
-    />
-    <div>
-      type:{{ type }}
-      <br />
-      interval:{{ interval }}
-    </div>
+    <ol>
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">
+          {{ beautify(group.title) }}<span>￥{{ group.total }}</span>
+        </h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -23,15 +27,82 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
-import intervalLis from "@/constants/intervalLis";
+import dayjs from "dayjs";
+import clone from "@/lib/clone";
 
 @Component({
   components: { Tabs },
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "-" : tags.join(",");
+  }
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, "day")) {
+      return "今天";
+    } else if (day.isSame(now.subtract(1, "day"), "day")) {
+      return "昨天";
+    } else if (day.isSame(now.subtract(2, "day"), "day")) {
+      return "前天";
+    } else if (day.isSame(now, "year")) {
+      return day.format("M月D日");
+    } else {
+      return day.format("YYYY年M月D日");
+    }
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get groupedList() {
+    const { recordList } = this;
+    if (recordList.length === 0) {
+      return [];
+    }
+    type HashTableValue = { title: string; items: RecordItem[] };
+
+    const newList = clone(recordList)
+      .filter((r) => r.type === this.type)
+      .sort(
+        (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
+      );
+
+    type Result = { title: string; total?: number; items: RecordItem[] };
+    if (newList.length === 0) {
+      return [] as Result[];
+    }
+    const result: Result[] = [
+      {
+        title: dayjs(newList[0].createdAt).format("YYYY-MM-DD"),
+        items: [newList[0]],
+      },
+    ];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), "day")) {
+        last.items.push(current);
+      } else {
+        result.push({
+          title: dayjs(current.createdAt).format("YYYY-MM-DD"),
+          items: [current],
+        });
+      }
+    }
+    result.forEach((group) => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchRecords");
+  }
+
   type = "-";
-  interval = "day";
-  intervalList = intervalLis;
   recordTypeList = recordTypeList;
 }
 </script>
@@ -39,10 +110,10 @@ export default class Statistics extends Vue {
 <style lang="scss" scoped>
 ::v-deep {
   .type-tabs-item {
-    background-color: #fff;
+    background-color: #c4c4c4;
 
     &.selected {
-      background-color: #c4c4c4;
+      background-color: #fff;
       &::after {
         display: none;
       }
@@ -51,5 +122,24 @@ export default class Statistics extends Vue {
   .interval-tabs-item {
     height: 48px;
   }
+}
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  background: #fff;
+  @extend %item;
+}
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
 }
 </style>
